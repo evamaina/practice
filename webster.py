775569769e3cac4ccee0a11406940c96012e0ed0
Eva <evajohnson714@gmail.com>
2017-10-18 01:15:05 +0300

    Print out the values in the dictionery using a for loop

---
+++ webster.py
@@ -0,0 +1,10 @@
+webster = {
+  "Aardvark" : "A star of a popular children's cartoon show.",
+  "Baa" : "The sound a goat makes.",
+  "Carpet": "Goes on the floor.",
+  "Dab": "A small amount."
+}
+
+""""Use a for loop to go through the webster dictionary and print out all of the definitions."""
+for i in webster:
+  print webster[i]
