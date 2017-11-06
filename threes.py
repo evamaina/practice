edc055301c8f0a02bcd1d9c8cffd665b5bd660b5
Eva <evajohnson714@gmail.com>
2017-11-06 09:44:54 +0300

    print numbers divisible by 3 and 4

---
+++ threes.py
@@ -0,0 +1,6 @@
+threes_and_fives = [x for x in range(1, 16) if x % 3 == 0 or x % 5 == 0]
+print threes_and_fives
+
+"""Use a list comprehension to create a list, threes_and_fives,
+ that consistsonly of the numbers between 1 and 15 (inclusive)
+ that are evenly divisible by 3 or 5."""
