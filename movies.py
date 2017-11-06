c37efb5b897d8e064aeb75d146eaffdf59770a12
Eva <evajohnson714@gmail.com>
2017-11-06 09:36:05 +0300

    print the all items in the dictionery

---
+++ movies.py
@@ -0,0 +1,10 @@
+movies = {
+  "Monty Python and the Holy Grail": "Great",
+  "Monty Python's Life of Brian": "Good",
+  "Monty Python's Meaning of Life": "Okay"
+}
+print movies.items()
+
+"""Call the appropriate method on movies
+such that it will print out all the items
+ in the dictionary, each key and each value."""
