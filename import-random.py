3c7289001c76ac0b3fff100fcdf4c998e020a256
Eva <evajohnson714@gmail.com>
2017-11-07 09:53:19 +0300

    using random module

---
+++ import-random.py
@@ -0,0 +1,9 @@
+import random
+for i in range(5):
+    print(random.randint(1, 10))
+
+    """The random randint function call evaluates
+     to a random integer valuebetween the two integers
+     that you pass it. Since randint() is in the randommodule,
+     you must first type random. in front of the function name to tell
+     Python to look for this function inside the random module."""
