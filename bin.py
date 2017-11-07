1de04fcc571dae79a0445494fc7f88c14bb5b9e9
Eva <evajohnson714@gmail.com>
2017-11-07 08:12:41 +0300

    Using bin() to print numbers in binary representation

---
+++ bin.py
@@ -0,0 +1,16 @@
+"""use print and bin() to print out the binary
+ representations of the numbers 1 through 5,
+  each on its own line."""
+print bin(1)
+print bin(2)
+print bin(3)
+print bin(4)
+print bin(5)
+
+
+""" In order to print a number in its binary representation,
+ you can use the bin() function. bin() takes an integer as
+ input and returns the binary representation of that integer in a string.
+  (Keep in mind that after using the bin function, you can no longer
+  operate on the value like a number.)You can also represent numbers
+  in base 8 and base 16 using the oct() and hex() functions."""
